e61bc5f77f7ed9b6e7c28760a9d165c9f7244397
mygitcode1 <36421857+mygitcode1@users.noreply.github.com>
2021-10-20 14:53:28 +0530

    Update clientinstall.sh

--- clientinstall.sh
+++ clientinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum -y install openldap-clients nss-pam-ldapd
+apt -y install openldap-clients nss-pam-ldapd
 read -p 'Enter LDAP Server IP Address: ' ip
-authconfig --enableldap --enableldapauth --ldapserver=$ip --ldapbasedn="dc=linuxautomations,dc=com" --enablemkhomedir --update
+authconfig --enableldap --enableldapauth --ldapserver=$ip --ldapbasedn="dc=users,dc=automations,dc=com" --enablemkhomedir --update
